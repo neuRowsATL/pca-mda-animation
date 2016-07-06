9f2e9cc4d77d3a49e72733e0a55ad66bb5dc16d2
Robert Capps <robbie@RobbiesMBP.local>
2016-07-06 17:30:28 -0400

    changes from meeting today

--- freqpy/find_trajectories.py
+++ freqpy/find_trajectories.py
@@ -19,7 +19,7 @@ pd = pairwise_distances(proj, metric='l2')
 pd /= np.max(pd)
 # opd = pd.copy()
 
-eps = np.mean(pd) + np.std(pd)*(-0.0)
+eps = np.mean(pd) + np.std(pd)*(-0.5)
 
 bpdw = np.where(pd >= eps)
 gpdw = np.where(pd < eps)
@@ -28,10 +28,10 @@ pd[gpdw] = 1
 pd[bpdw] = 0
 np.fill_diagonal(pd, 0)
 
-# fig = plt.figure()
-# ax = plt.subplot()
-# ax.plot(np.linspace(0, pd.shape[0]), np.linspace(0, pd.shape[0]))
-# ax.matshow(-pd, cmap=plt.cm.gray)
+fig = plt.figure()
+ax = plt.subplot()
+ax.plot(np.linspace(0, pd.shape[0]), np.linspace(0, pd.shape[0]))
+ax.matshow(pd, cmap=plt.cm.gray)
 
 def kth_diag_indices(a, k):
     rows, cols = np.diag_indices_from(a)
@@ -50,42 +50,44 @@ def consec(condition):
     return cw, cc
 
 def tconsec(diag):
-    done = list()
-    for di, dd in enumerate(diag):
-        if dd == 1 and di not in done:
-            xx = 0
-            try:
-                while diag[di+xx] == dd:
-                    done.append(di+xx)
-                    xx += 1
-            except IndexError:
-                continue
-    return done
+    out = list()
+    ix = 0
+    while ix < len(diag):
+        chain = list()
+        if ix >= len(diag): break
+        while diag[ix] == 1:
+            chain.append(ix)
+            ix += 1
+            if ix >= len(diag): break
+        out.append(chain)
+        ix += 1
+    return out
 
-len_check = -50
+len_check = 15
 cnt = 0
 npd = pd.copy()
-npd.fill(0)
+# npd.fill(0)
+orng = pd.shape[0]
 rows = list()
 cols = list()
-orng = pd.shape[0]
 for col_offset in range(orng):
-    consec_rows = tconsec(pd.diagonal(col_offset, 1, 0))
-    nncol = np.arange(col_offset, col_offset+len(consec_rows)).tolist()
-    for cr, ccol in zip(consec_rows, nncol):
-        if all([mm >= abs(len_check) for mm in [cr, ccol]]):
-            if all([pd[cr+xx, ccol+xx] == 1 for xx in range(len_check, 0, 1)]):
-                cnt += 1
-                npd[cr, ccol] = 255
-                # circle2=plt.Circle((ccol, cr), len_check, color='w', fill=False)
-                # fig.gca().add_artist(circle2)
-
+    consec = tconsec(pd.diagonal(col_offset))
+    for con in consec:
+        if len(con) >= len_check:
+            cnt += 1
+            rows.append((con[0], con[-1]))
+            cols.append((con[0] + col_offset, con[-1] + col_offset))
+            for r in con:
+                c = col_offset + r
+                npd[r, c] = 255
+    # circle2=plt.Circle((ccol, cr), len_check, color='w', fill=False)
+    # fig.gca().add_artist(circle2)
 
 print cnt
 
 # ax = plt.subplot()
-# ax.plot(np.linspace(0, pd.shape[0]), np.linspace(0, pd.shape[0]))
-# ax.matshow(npd, cmap=plt.cm.jet, alpha=0.5)
+ax.plot(np.linspace(0, pd.shape[0]), np.linspace(0, pd.shape[0]))
+ax.matshow(npd, cmap=plt.cm.jet, alpha=0.9)
 
 threeD = True
 if threeD is True:
@@ -93,25 +95,57 @@ if threeD is True:
     labels = np.loadtxt('pdat_labels.txt')
     ax = plt.subplot(projection='3d')
 
-    ii = 0
-    for rix in range(npd.shape[0]):
-        for cix in range(npd.shape[1]):
-            color = colorlist.keys()[ii % len(colorlist.keys())]
-            pc1 = proj[rix, :]
-            pc2 = proj[cix, :]
-            if npd[rix, cix] == 255:
-                ax.plot([pc1[0], pc2[0]], 
-                        [pc1[1], pc2[1]], 
-                        zs=[pc1[2], pc2[2]], 
-                        lw=1.0, color=color,marker='o')
-                if npd[rix-1,cix-1] == npd[rix+1,cix+1]:
-                    ii += 1
+    ccc = 0
+    colors = ['r', 'b']
+    ax.plot(proj[:, 0], proj[:, 1], proj[:, 2], alpha=0.05)
+    done = list()
+    for r, c in zip(rows, cols):
+        # if ccc in [18]:
+        if ccc >= 0:
+            done.append((r, c))
+            pc1 = proj[r[0]:r[1], :]
+            pc2 = proj[c[0]:c[1], :]
+            print r, c
+            # print pproj.shape, pproj2.shape
+            for p1, p2 in zip(pc1, pc2):
+                ax.plot([p1[0], p2[0]], 
+                        [p1[1], p2[1]], 
+                        zs=[p1[2], p2[2]], 
+                        lw=1.0,marker='',color='g',alpha=0.5)
+            ax.plot(pc1[:, 0], pc1[:, 1], zs=pc1[:, 2], lw=3.0, marker='o',color='r', alpha=1.)
+            ax.plot(pc2[:, 0], pc2[:, 1], zs=pc2[:, 2], lw=3.0, marker='o',color='b', alpha=1.)
+        ccc += 1
+
+    # end = False
+    # ii = 0
+    # prev = 255
+    # for rix in range(npd.shape[0]):
+    #     for cix in range(npd.shape[1]):
+    #         # color = colorlist.keys()[ii % len(colorlist.keys())]
+    #         pc1 = proj[rix, :]
+    #         pc2 = proj[cix, :]
+    #         if npd[rix, cix] == 255:
+    #             ax.plot([pc1[0], pc2[0]], 
+    #                     [pc1[1], pc2[1]], 
+    #                     zs=[pc1[2], pc2[2]], 
+    #                     lw=1.0,marker='o')
+    #             if prev == npd[rix, cix]:
+    #                 prev = npd[rix, cix]
+    #                 ii += 1
+    #             else:
+    #                 ii = 0
+    #             if ii == 20:
+    #                 ii = 0
+    #                 end = True
+    #                 break
+    #     if end == True:
+    #         break
 
     plt.title("DS Orbits Manifold")
     plt.xlabel("PC1")
     plt.ylabel("PC2")
     ax.set_zlabel("PC3")
-    plt.savefig('DS_orbits_manifold.png')
+    # plt.savefig('DS_orbits_manifold.png')
 
 plt.show()
 
